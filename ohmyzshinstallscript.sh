fc1423ed357bb271200c63b96efd24c4b15013a8
Kristopher Bright <drlucky@tutanota.com>
2023-04-14 18:03:13 -0400

    added ohmyzsh script

---
+++ ohmyzshinstallscript.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##install zsh and zsh-completions
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+wait $!
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+wait $!
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+wait $!
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="gnzh"/' ~/.zshrc
+wait $!
+sed -i 's/plugins=(git)/plugins=(git archlinux zsh-syntax-highlighting zsh-autosuggestions)/' ~/.zshrc
+wait $!
+echo "set -o vi" >> ~/.zshrc
+wait $!
+echo "export EDITOR=/usr/bin/vim" >> ~/.zshrc
+
+
